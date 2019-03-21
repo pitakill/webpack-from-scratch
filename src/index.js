4d6a0be77e45f650c74cb8c441c0f280cf00d2ab
Lepoldo Caballero <git@pitakill.net>
2019-03-20 22:10:02 -0600

    [feat] Use react in the project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+import Test from './components/Test'
 import './style.scss'
+
+render(<Test />, document.getElementById("root"))
--- src/utilities.js
+++
@@ -1,5 +0,0 @@
-const p = document.createElement('p')
-const text = document.createTextNode('Hola Mundo')
-
-p.appendChild(text)
-document.body.appendChild(p)
