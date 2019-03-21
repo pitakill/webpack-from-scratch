8ec60ed3d752bf6a057aa174a5a9b9d96a73585d
Lepoldo Caballero <git@pitakill.net>
2019-03-20 19:46:17 -0600

    [feat] File-loader works in {S,}CSS files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
 import example from './example'
 import './style.scss'
-import image from './background-640x320.jpg'
-
-const background = new Image()
-background.src = image
-
-document.body.appendChild(background)
 
 example()
